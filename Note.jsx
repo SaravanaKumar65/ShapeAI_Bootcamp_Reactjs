3db70381cec1ad11775dc106ddbf6f68b38e316c
Saravana Kumar R <78146697+SaravanaKumar65@users.noreply.github.com>
2021-06-06 11:39:37 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,171 @@
+import React from "react";
+
+function Note() {
+  return (
+    <>
+      <div className="note">
+        <h1>MS DHONI (C)</h1>
+        <p>
+          A batting powerhouse, MS Dhoni possesses the ability to destroy
+          different bowling attacks on his own. The calmness and composure in
+          his demeanor along with a sharp cricketing brain accounts for a lethal
+          combination on the cricketing field and the glistening silverware in
+          the trophy cabinets at CSK are a testimony to his extraordinary
+          leadership skills.
+        </p>
+      </div>
+      <div className="note">
+        <h1>Suresh Raina</h1>
+        <p>
+          Second retained player by CSK ahead of the IPL Auction 2018, Suresh
+          Raina is a visual treat for the spectators in this shortest format of
+          the game. A solid striker of the ball, he possesses the rare quality
+          of performing well under pressure. Batting in the top order, Raina has
+          mastered the art of finishing games.The left-handed batsman is also a
+          part-time off-break bowler.
+        </p>
+      </div>
+      <div className="note">
+        <h1>Ravindra Jadeja</h1>
+        <p>
+          The third retained player by CSK ahead of the IPL 2018 Player Auction,
+          Ravindra Jadeja has been MS Dhoni’s go-to-man when he needed to break
+          a partnership and the left-armer has delivered the goods on many
+          occasions. His presence gives Dhoni the extra cushion of playing an
+          additional batsman if needed. Jadeja has been an enigma right since
+          his advent.
+        </p>
+      </div>
+      <div className="note">
+        <h1>Ruturaj Gaikwad</h1>
+        <p>
+          DOB 31 January 1997 A 22-year old opening batsman who hails from Pune,
+          Ruturaj Gaikwad has been a consistent performer for his state team
+          Maharashtra. He was the highest run-getter for Maharashtra in the
+          league stage of the Syed Mushtaq Ali Trophy 2018-19. At the IPL 2019
+          Player Auction, the Chennai Super Kings secured his services at his
+          base price of INR 20 lacs.
+        </p>
+      </div>
+      <div className="note">
+        <h1>Ambati Rayudu</h1>
+
+        <p>
+          A prolific run-getter since his early days in the game, AmbatiRayudu
+          plied his trade at Mumbai Indians last time around and now he will get
+          a chance to play under MS Dhoni this season. Once referred to as
+          India’s next batting star, Rayudu announced his arrival when he scored
+          177 as a 16-year-old against an England U-19 team in 2002. Stints at
+          the National Cricket Academy.
+        </p>
+      </div>
+      <div className="note">
+        <h1>Dwayne Bravo</h1>
+        <p>
+          Dwayne Bravo is one of the most entertaining players in the world. He
+          brings in Caribbean swagger to the IPL and bats with typical West
+          Indian flair with lovely shots all round the wicket. Having played
+          plenty of Twenty20 leagues across the world, Bravo encapsulates the
+          knowledge of the game and also has a good grasp on the technicalities
+          of the game.
+        </p>
+      </div>
+      <div className="note">
+        <h1>Deepak Chahar</h1>
+        <p>
+          The Agra-born medium pacer, Deepak Chahar was a part of the Rajasthan
+          Royals squad in 2012, donned the RPS jersey for two seasons and is now
+          all set to represent the Chennai Super Kings in the eleventh edition
+          of the league. Young Chahar plays for the state side Rajasthan in
+          domestic cricket. In the season-opener against Hyderabad in 2010-11.
+        </p>
+      </div>
+      <div className="note">
+        <h1>Faf du Plessis</h1>
+        <p>
+          Faf Du Plessis will also once again don the yellow of Chennai Super
+          Kings colours after they used the Right to Match card for him. KXIP
+          bought him for Rs 1.6 Cr but CSK trumped them by using their RTM card
+          and it is a bargain. While the classy and composed batsman did not get
+          many outings in the RPS squad where he was picked up in the IPL player
+          draft.Plessis has had consistent .
+        </p>
+      </div>
+      <div className="note">
+        <h1>Sam Curran</h1>
+        <p>
+          Sam Curran was the surprise weapon through which England beat India in
+          the Test series last year; the all-rounder was named Man of the Series
+          by the Indian team management. He was rewarded with an eye-watering
+          INR 7.2 crore deal by Kings XI Punjab, the third-highest bid in the
+          auctions prior to IPL 2019. The ICC named him one of the top five
+          breakout stars of 2018.
+        </p>
+      </div>
+      <div className="note">
+        <h1>Moeen Ali</h1>
+        <p>
+          A versatile left-handed batsman and an aggressive off-break bowler,
+          Moeen Ali was bought by Chennai Super Kings for INR 7 crores at the
+          IPL 2021 Player Auction. The left-hander represented RCB for two
+          seasons before he was released by the franchise ahead of the 2021
+          edition of the league. Come the fourteenth edition of the league, the
+          English all-rounder.
+        </p>
+      </div>
+      <div className="note">
+        <h1>Imran Tahir</h1>
+        <p>
+          At 37 years of age, Imran Tahir became the World No.1 bowler in the
+          shortest format of the game. His positivity and never-say-die attitude
+          have been his success mantra all through these years, which is on
+          display, every time he takes to the field. From his customary sprint,
+          after he picks up a wicket to the roars, Tahir’s enthusiasm has kept
+          him going.
+        </p>
+      </div>
+      <div className="note">
+        <h1>Mitchell Santner</h1>
+        <p>
+          World No. 1 in T20 format, Mitchell Santner is all set to make his
+          debut in the league for Chennai Super Kings. Bought by the CSK for
+          just INR 50 lacs, the Kiwi bowler has to be one of the most economical
+          buys for CSK. Santner made his debut for New Zealand when Daniel
+          Vettori retired from his ranks and in only his first game.
+        </p>
+      </div>
+      <div className="note">
+        <h1>Karn Sharma</h1>
+        <p>
+          A leg-spinner from Uttar Pradesh, Karn Sharma made headlines in IPL
+          2014 when Sunrisers Hyderabad bought him for INR 3.75 cr to in the IPL
+          Auction. It was the highest amount earned by a domestic uncapped
+          player in the auction and was a reward for his performance with SRH
+          when he grabbed 11 wickets in 13 games at an economy rate of 6.60.
+        </p>
+      </div>
+      <div className="note">
+        <h1>Robin Uthappa</h1>
+        <p>
+          The unfailingly consistent and proven match-winner, Robin Uthappa, who
+          has scored 4607 IPL runs, was traded to Chennai Super Kings by the
+          Rajasthan Royals ahead of IPL 2021 Player Auction. Uthappa had joined
+          the Royals ahead of the 2020 season and played 12 games for them.
+          Earlier, Uthappa was one of KKR’s star-turns in the title-winning.
+        </p>
+      </div>
+      <div className="note">
+        <h1>Shardul Thakur</h1>
+        <p>
+          It was the Qualifier 1 of the tenth edition of the league between the
+          Mumbai Indians and Rising Pune Supergiant at the Wankhede Stadium, and
+          it was none other than the local quick who picked up three wickets and
+          helped his team reach the final of the tournament. Shardul Thakur, who
+          has been consistently among wickets for the Mumbai Ranji team.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Note;
